de0a97eb26c3ca686376b25140b6b84691afefd4
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 20:05:06 +0300

    :bug: Cover deployment edge case

--- database/config/config.js
+++ database/config/config.js
@@ -1,5 +1,6 @@
 const LOCALHOST_DB_POSTGRESQL_URL = 'postgres://postgres@localhost:5432/node-friends-app';
 
 module.exports = {
+  /* istanbul ignore next: because it is normal during deployment to always have a set `DB_POSTGRESQL_URL` */
   url: process.env.DB_POSTGRESQL_URL || LOCALHOST_DB_POSTGRESQL_URL,
 };
