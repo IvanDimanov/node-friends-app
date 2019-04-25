3e26a4733fd4a5820417c8dac3d54a9f999148e9
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 16:06:47 +0300

    Test

--- database/config/sequelize-pool.js
+++ database/config/sequelize-pool.js
@@ -5,7 +5,13 @@ const config = require('../config/config');
 
 Sequelize.formatLogging = (sql) => sql.replace(/[\r\n\t]+/g, ' ').replace(/\s+/g, ' ');
 
-const getInstance = () => {};
+const instance = {};
+
+instance.id = uuid();
+instance.types = Sequelize;
+instance.formatLogging = Sequelize.formatLogging;
+
+const getInstance = () => instance;
 
 module.exports = {
   Sequelize,
