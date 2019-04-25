cd3b22d1f97965b837b18c899f81708418823da5
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 17:42:59 +0300

    Test

--- backend/koa-middleware/applyAllRoutes.js
+++ backend/koa-middleware/applyAllRoutes.js
@@ -6,14 +6,15 @@ const DEFAULT_ROUTES_FOLDER = '../routes';
 const usersRouter = require('../routes/users');
 const groupsRouter = require('../routes/groups');
 
-const basePath = path.join(__dirname, DEFAULT_ROUTES_FOLDER);
-const routers = fs.readdirSync(basePath)
-    .filter((itemPath) => {
-      /* Ignore test files and folders */
-      return itemPath !== 'test' && !itemPath.includes('.spec.');
-    })
-    .map((itemPath) => require(path.join(basePath, itemPath)));
+async function applyAllRoutes(app) {
+  app
+    .use(usersRouter.routes())
+    .use(usersRouter.allowedMethods());
 
+  app
+    .use(groupsRouter.routes())
+    .use(groupsRouter.allowedMethods());
+}
 
 /**
  * Loads all JS modules from the `routesFolderPath` and applies them to the koa `app`
@@ -23,18 +24,8 @@ const routers = fs.readdirSync(basePath)
  * @param {object} app Instance of `new Koa()`
  * @param {string} routesFolderPath Folder that host all BackEnd API routes
  */
-async function applyAllRoutes(app, routesFolderPath = DEFAULT_ROUTES_FOLDER) {
+async function applyAllRoutes2(app, routesFolderPath = DEFAULT_ROUTES_FOLDER) {
   const basePath = path.join(__dirname, routesFolderPath);
-
-  if (routesFolderPath === DEFAULT_ROUTES_FOLDER) {
-    routers.forEach((router) => {
-      app
-          .use(router.routes())
-          .use(router.allowedMethods());
-    });
-    return;
-  }
-
   fs.readdirSync(basePath)
       .forEach((itemPath) => {
         /* Ignore test files and folders */
