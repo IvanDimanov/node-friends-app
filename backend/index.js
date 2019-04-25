89e4788e9101bced26cdc8b28f7413136a46dbc9
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 14:27:01 +0300

    Test

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,21 @@ const helmet = require('koa-helmet');
 const jwt = require('koa-jwt');
 const cors = require('@koa/cors');
 
+const catchError = require('./koa-middleware/catchError');
+const logger = require('./koa-middleware/logger');
+const jwtToUser = require('./koa-middleware/jwtToUser');
+const notFound = require('./koa-middleware/notFound');
+const onError = require('./koa-middleware/onError');
+const {applyAllRoutes} = require('./koa-middleware/applyAllRoutes');
+
+const postgres = require('../database/models');
 
 const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World 11\n');
+  res.end(`Hello World 11 ${Date.now()}\n`);
 });
 
 server.listen(port, () => {
