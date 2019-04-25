7aae7ded8fe7b3f7fd29b223e9add8d54dfc20fe
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 16:12:22 +0300

    Test

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,46 @@ const cors = require('@koa/cors');
 const catchError = require('./koa-middleware/catchError');
 const logger = require('./koa-middleware/logger');
 const jwtToUser = require('./koa-middleware/jwtToUser');
+const notFound = require('./koa-middleware/notFound');
+const onError = require('./koa-middleware/onError');
+const {applyAllRoutes} = require('./koa-middleware/applyAllRoutes');
+
+const postgres = require('../database/models');
 
 const port = process.env.PORT || 8000;
+
+/**
+ * Creates the main Koa app with all middlewares, APIs, logging, and error handling.
+ *
+ * @category BackEnd
+ *
+ * @return {Object} Instance of Koa
+ */
+function createApp() {
+  const app = new Koa();
+
+  app.context.postgres = postgres;
+
+  app
+      .use(helmet())
+      .use(catchError())
+      .use(logger)
+      .use(bodyParser())
+      .use(jwt({secret: process.env.JWT_SECRET || 'Pass@123', key: 'jwtdata', passthrough: true}))
+      .use(jwtToUser);
+
+  if (process.env.ALLOW_CORS) {
+    app.use(cors());
+  }
+
+  applyAllRoutes(app);
+
+  app
+      .use(notFound)
+      .on('error', onError);
+
+  return app;
+}
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
--- backend/koa-middleware/jwtToUser.js
+++ backend/koa-middleware/jwtToUser.js
@@ -24,8 +24,33 @@ async function jwtToUser(ctx, next) {
     return next();
   }
 
-  ctx.state.user = {};
-  ctx.state.userPermissions = [];
+  ctx.state.user = await ctx.postgres.Users
+      .findOne({
+        where: {
+          id: ctx.state.jwtdata.userId,
+        },
+      })
+      .then((response) => response && typeof response.toJSON === 'function' ? response.toJSON() : {});
+
+  const permissionIds = await ctx.postgres.UserPermissions
+      .findAll({
+        attributes: ['permissionId'],
+        where: {
+          userId: ctx.state.jwtdata.userId,
+        },
+      })
+      .then((responses) => Array.isArray(responses) ? responses.map(({permissionId}) => permissionId) : []);
+
+  ctx.state.userPermissions = await ctx.postgres.Permissions
+      .findAll({
+        attributes: ['key'],
+        where: {
+          id: {
+            [Sequelize.Op.in]: permissionIds,
+          },
+        },
+      })
+      .then((responses) => Array.isArray(responses) ? responses.map(({key}) => key) : []);
 
   return next();
 }
