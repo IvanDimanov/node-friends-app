e58385e5dccf5fadbf506cf49fa12e875743b137
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 16:03:13 +0300

    Test

--- backend/koa-middleware/jwtToUser.js
+++ backend/koa-middleware/jwtToUser.js
@@ -1,3 +1,17 @@
+const {Sequelize} = require('../../database/config/sequelize-pool');
+
+/**
+ * if some of the previous Koa middlewares loaded the JWT into `ctx.state.jwtdata`
+ * then this middleware loads user data and user permissions from the DB
+ * into `ctx.state.user` and `ctx.state.userPermissions`.
+ *
+ * @category middleware
+ *
+ * @param {Object} ctx Koa context
+ * @param {Function} next Koa next middleware
+ *
+ * @return {Function} In the end, calls `next`
+ */
 async function jwtToUser(ctx, next) {
   if (!ctx.state) {
     ctx.state = {};
