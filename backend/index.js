f39ca6e96a79fed5f374420711e3f631e6b04fb7
Ivan Dimanov <ivan.dimanov@xogito.com>
2019-04-25 14:24:29 +0300

    Test

--- backend/index.js
+++ backend/index.js
@@ -5,42 +5,8 @@ const helmet = require('koa-helmet');
 const jwt = require('koa-jwt');
 const cors = require('@koa/cors');
 
-const catchError = require('./koa-middleware/catchError');
-const logger = require('./koa-middleware/logger');
-const jwtToUser = require('./koa-middleware/jwtToUser');
-const notFound = require('./koa-middleware/notFound');
-const onError = require('./koa-middleware/onError');
-const {applyAllRoutes} = require('./koa-middleware/applyAllRoutes');
-
-// const postgres = require('../database/models');
 
 const port = process.env.PORT || 8000;
-
-
-
-// const app = new Koa();
-
-// app.context.postgres = postgres;
-
-// app
-//     .use(helmet())
-    // .use(catchError())
-    // .use(logger)
-    // .use(bodyParser())
-    // .use(jwt({secret: process.env.JWT_SECRET || 'Pass@123', key: 'jwtdata', passthrough: true}))
-    // .use(jwtToUser);
-
-// if (process.env.ALLOW_CORS) {
-//   app.use(cors());
-// }
-
-// applyAllRoutes(app);
-
-// app
-//     .use(notFound)
-//     .on('error', onError);
-
-
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
